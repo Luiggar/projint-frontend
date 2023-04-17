ec2fe79ff52149e10be10c65a259fbe59b261b84
Luigi <luiz.gomes.garcia@gmail.com>
2023-04-17 18:39:55 -0300

    Front end comir

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ export default class Navbar extends Component {
     return (
       <><div className="bg-gray-800">
         <div className='h-16 px-8 flex items-center'>
-          <p className='text-white font-bold'>Management Tools</p>
+          <p className='text-white font-bold'>Management Tools System</p>
         </div>
       </div><p className=' h-16'></p></>
     )
